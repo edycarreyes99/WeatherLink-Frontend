ff455cb4f4ea955f1b47c4f7a9784f7906d9a8a7
Edycar Reyes <edycarreyes@gmail.com>
2020-10-17 19:15:44 -0600

    Se configura la conexion al backend para mostrar correctamente con datos reales el grafico de la temperatura

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -69,6 +69,8 @@ window.initMap = function () {
 
     localStorage.setItem("ultima-actualizacion-kpi", new Date())
 
+    generarGraficoTemperatura();
+
     // Verificador para determinar si de sebe consumir la API para actualizar los KPI o no cada 5 minutos
     setInterval(() => {
         let lastDate = new Date(localStorage.getItem("ultima-actualizacion-kpi"))
@@ -81,78 +83,85 @@ window.initMap = function () {
     }, 5000);
 }
 
-// Se inicializa el grafico para la temperatura
-Highcharts.chart('grafico-temperatura', {
-    chart: {
-        type: 'column',
-    },
-    exporting: {
-        enabled: false
-    },
-    title: {
-        text: 'Pronóstico de Temperatura °C',
-        align: 'left',
-        style: {
-            color: '#40B8F0'
+// Metodo para generar el grafico de la temperatura
+function generarGraficoTemperatura() {
+    firebaseApp.app().auth().onAuthStateChanged((user) => {
+        if (user) {
+            user.getIdToken(true).then((token) => {
+                const config = {
+                    method: 'get',
+                    url: 'https://localhost:5001/GenerarDatosParaGraficoDeTemperaturas/',
+                    headers: {
+                        'Authorization': 'Bearer ' + token
+                    }
+                };
+
+                axios(config)
+                    .then(function (response) {
+                        const series = response.data['data']['series'];
+                        const categories = response.data['data']['categories']
+                        // Se inicializa el grafico para la temperatura
+                        Highcharts.chart('grafico-temperatura', {
+                            loading:{
+                                hideDuration: 100,
+                                showDuration: 100
+                            },
+                            chart: {
+                                type: 'column',
+                            },
+                            exporting: {
+                                enabled: false
+                            },
+                            title: {
+                                text: 'Pronóstico de Temperatura °C',
+                                align: 'left',
+                                style: {
+                                    color: '#40B8F0'
+                                }
+                            },
+                            yAxis: {
+                                min: 0,
+                                max: 40,
+                                title: ""
+                            },
+                            xAxis: {
+                                categories,
+                                crosshair: true
+                            },
+                            legend: {
+                                layout: "horizontal",
+                                align: "right",
+                                verticalAlign: "top",
+                                squareSymbol: true,
+                                symbolRadius: 0,
+                            },
+                            tooltip: {
+                                headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+                                pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+                                    '<td style="padding:0"><b>{point.y:.1f}°C de temperatura.</b></td></tr>',
+                                footerFormat: '</table>',
+                                shared: true,
+                                useHTML: true
+                            },
+                            series,
+                            credits: {
+                                enabled: false
+                            }
+                        });
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+            }).catch((error) => {
+                usertoken = null;
+            });
+        } else {
+            console.error("Necesita estar logueado para ver este contenido.");
+            location.replace('/index.html');
+            usertoken = null;
         }
-    },
-    yAxis: {
-        min: 0,
-        max: 40,
-        title: ""
-    },
-    xAxis: {
-        categories: [
-            '8 Oct',
-            '9 Oct',
-            '10 Oct',
-            '11 Oct',
-            '12 Oct'
-        ],
-        crosshair: true
-    },
-    legend: {
-        layout: "horizontal",
-        align: "right",
-        verticalAlign: "top",
-        squareSymbol: true,
-        symbolRadius: 0,
-    },
-    tooltip: {
-        headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
-        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-            '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
-        footerFormat: '</table>',
-        shared: true,
-        useHTML: true
-    },
-    series: [
-        {
-            name: 'Estacion 1',
-            data: [49.9, 71.5, 106.4, 129.2, 144.0],
-            marker: {symbol: 'square', radius: 12}
-        }, {
-            name: 'Estacion 2',
-            data: [83.6, 78.8, 98.5, 93.4, 106.0],
-            marker: {symbol: 'square', radius: 12}
-        }, {
-            name: 'Estacion 3',
-            data: [48.9, 38.8, 39.3, 41.4, 47.0],
-            marker: {symbol: 'square', radius: 12}
-        }, {
-            name: 'Estacion 4',
-            data: [42.4, 33.2, 34.5, 39.7, 52.6],
-            marker: {symbol: 'square', radius: 12}
-        }, {
-            name: 'Estacion 5',
-            data: [42.4, 33.2, 34.5, 39.7, 52.6],
-            marker: {symbol: 'square', radius: 12}
-        }
-    ],
-    credits: {
-        enabled: false
-    }
-});
+    });
+}
 
 // Se inicializa el grafico para la humedad
 Highcharts.chart('grafico-humedad', {
@@ -186,11 +195,11 @@ Highcharts.chart('grafico-humedad', {
     },
     xAxis: {
         categories: [
-            '8 Oct',
-            '9 Oct',
-            '10 Oct',
-            '11 Oct',
-            '12 Oct'
+            new Date(),
+            new Date(),
+            new Date(),
+            new Date(),
+            new Date()
         ],
         crosshair: true
     },
@@ -207,10 +216,18 @@ Highcharts.chart('grafico-humedad', {
             }
         }
     },
+    tooltip: {
+        headerFormat: `<span style="font-size:10px">{point.key}</span><table>`,
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f}% de humedad.</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
     series: [
         {
             name: 'Estacion 1',
-            data: [20, 30, 70, 80, 97031]
+            data: [20, 30, 70, 80, 97]
         },
         {
             name: 'Estacion 2',
